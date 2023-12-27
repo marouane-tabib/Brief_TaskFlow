68923f0d5c9e45748ef7de61f410bbfc3deca355
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-27 08:52:22 +0100

    First Commit...

---
+++ src/main/java/com/example/brief_taskflow/BriefTaskFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.brief_taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefTaskFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BriefTaskFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brief_taskflow/BriefTaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brief_taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefTaskFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
